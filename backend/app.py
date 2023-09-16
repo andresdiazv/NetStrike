69c34bc4fb92360031dd449cc0997528a44d4246
Andres Diazvictores <adiaz616@fiu.edu>
2023-09-16 11:20:50 -0400

    changes

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import time
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@socketio.on('start_scan')
+def handle_start_scan(message):
+    ip_address = message['ip']
+    # Simulate a scan with progress updates
+    for progress in range(0, 101, 10):
+        scan_result = f"Scanning IP: {ip_address}, Progress: {progress}%"
+        socketio.emit('scan_update', {'update': scan_result, 'progress': progress})
+        time.sleep(1)  # Simulate time taken for scanning
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
